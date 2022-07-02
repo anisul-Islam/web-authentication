10a0f8cafd895d8b9862389bf673f85150ab9978
anisul-islam <anisul2010s@yahoo.co.uk>
2022-07-02 15:21:57 +0300

    Level 7: Passport jwt | token based authentication

--- Level 7: passport-jwt/server/index.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-
-const port = process.env.PORT || 4000;
-
-app.listen(port, () => {
-  console.log(`server is running at http://localhost:${port}`);
-});
